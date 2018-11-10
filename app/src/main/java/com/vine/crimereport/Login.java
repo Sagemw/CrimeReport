e6a9cd7be944df12ecd1d3ba22ba56e937ad7e03
sagemw <mbilawisdom@gmail.com>
2018-11-10 13:20:27 +0100

    Integrated firebase

--- app/src/main/java/com/vine/crimereport/Login.java
+++ app/src/main/java/com/vine/crimereport/Login.java
@@ -1,13 +1,134 @@
 package com.vine.crimereport;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+    
+    private String  email, password;
+    private EditText etEmail, etPassword;
+    private Button btnLogin, btnCreate_account;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCreate_account = findViewById(R.id.btnCreatAccount);
+
+        email = etEmail.getText().toString().toLowerCase().trim();
+        password = etPassword.getText().toString().trim();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validation();
+            }
+        });
+
+        btnCreate_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validation();
+            }
+        });
+
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Success", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Error", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Login .this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+    }
+
+    private void validation() {
+        if (isNetworkConnectionAvailable()){
+
+             if (TextUtils.isEmpty(email)) {
+            etEmail.setError("Email cannot be empty");
+            return;
+        }
+
+
+             if (password.length() <6){
+            etPassword.setError("Password cannot be empty");
+
+        }
+             else{
+            Toast.makeText(this, "successful", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(Login.this, Login.class));
+        }
+
+        }else{
+            Toast.makeText(this, "Put Data Connection", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // added as an instance method to an Activity
+    boolean isNetworkConnectionAvailable() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if (info == null) return false;
+        NetworkInfo.State network = info.getState();
+        return (network == NetworkInfo.State.CONNECTED || network == NetworkInfo.State.CONNECTING);
+    }
+
+
 }
+
+
+
+
